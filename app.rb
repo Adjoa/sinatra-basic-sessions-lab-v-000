5d130c8264a365319a9c1434746e9f6757687d96
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-23 20:08:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :Sessions
+    set :session_secret  "secret"
+  end
+  
   get '/' do
     erb :index
+  end
+
+  post 'checkout' do
+    erb :
   end
 
 end
