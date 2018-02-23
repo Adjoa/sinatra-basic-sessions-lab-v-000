033b0cb1e41a064b60fcb07a72bac608690bc11e
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-23 20:08:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,9 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   configure do
     enable :Sessions
-    set :session_secret  "secret"
+    set :session_secret, "secret"
   end
-  
+
   get '/' do
     erb :index
   end
